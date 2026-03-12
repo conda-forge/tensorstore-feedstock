d0b6f9b698ad3ab2aa9d22618db1dfc86411cda7
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2026-03-12 12:14:47 +0100

    Fix cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,40 +16,54 @@ if [[ $target_platform =~ osx.* && -d "$CONDA_BUILD_SYSROOT/usr/include/curl" ]]
     mv "$CONDA_BUILD_SYSROOT/usr/include/curl" "$CONDA_BUILD_SYSROOT/usr/include/curl.do-not-use"
 fi
 
-system_libs="com_google_boringssl"
-system_libs+=",org_sourceware_bzip2"
+system_libs="boringssl"
+system_libs+=",bzip2"
 system_libs+=",org_blosc_cblosc"
-system_libs+=",se_curl"
-system_libs+=",jpeg"
-system_libs+=",png"
+system_libs+=",curl"
+system_libs+=",libjpeg_turbo"
+system_libs+=",libpng"
 system_libs+=",libwebp"
-system_libs+=",org_lz4"
-system_libs+=",org_tukaani_xz"
-system_libs+=",net_zlib"
+system_libs+=",lz4"
+system_libs+=",xz"
+system_libs+=",zlib"
 system_libs+=",com_github_pybind_pybind11"
-system_libs+=",com_github_nlohmann_json"
+system_libs+=",nlohmann_json"
 system_libs+=",org_aomedia_avif"
-# system_libs+=",com_google_absl"
+# These names must match the Bazel repo names in third_party/*/workspace.bzl.
+# system_libs+=",abseil-cpp"
 export TENSORSTORE_SYSTEM_LIBS="$system_libs"
 
 build_options=""
 build_options+=" --crosstool_top=//bazel_toolchain:toolchain"
+build_options+=" --platforms=//bazel_toolchain:target_platform"
+build_options+=" --host_platform=//bazel_toolchain:build_platform"
+build_options+=" --extra_toolchains=//bazel_toolchain:cc_cf_toolchain"
+build_options+=" --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain"
 build_options+=" --logging=6"
 build_options+=" --verbose_failures"
 build_options+=" --toolchain_resolution_debug=.*"
 build_options+=" --enable_workspace"  # Bazel 8 compatibility: use WORKSPACE instead of MODULE.bazel
 build_options+=" --noenable_bzlmod"
+build_options+=" --define=with_cross_compiler_support=true"
 build_options+=" --local_cpu_resources=${CPU_COUNT}"
 build_options+=" --cpu=${TARGET_CPU}"
 build_options+=" --subcommands"  # comment out for debugging
 export TENSORSTORE_BAZEL_BUILD_OPTIONS="$build_options"
 
-# TODO: figure out why we need both TENSORSTORE_BAZEL_BUILD_OPTIONS and a bazelrc
 cat > .bazelrc <<EOF
-build --crosstool_top=//custom_toolchain:toolchain
+build --crosstool_top=//bazel_toolchain:toolchain
+build --platforms=//bazel_toolchain:target_platform
+build --host_platform=//bazel_toolchain:build_platform
+build --extra_toolchains=//bazel_toolchain:cc_cf_toolchain
+build --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain
 build --logging=6
 build --verbose_failures
+build --toolchain_resolution_debug=.*
+build --enable_workspace
+build --noenable_bzlmod
+build --define=with_cross_compiler_support=true
 build --local_cpu_resources=${CPU_COUNT}
+build --cpu=${TARGET_CPU}
 EOF
 
 # replace bundled baselisk with a simpler forwarder to our own bazel in build prefix
